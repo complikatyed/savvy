f123a14c26b1293084b8cce8e519d3692de59c8a
Alex Pearson <pearsonn.alex@gmail.com>
2015-09-04 04:36:07 +0000

    090315 save

---
+++ compress-static.pl
@@ -0,0 +1,43 @@
+#!/usr/local/bin/perl
+use warnings;
+use strict;
+# Set $verbose to a true value if you need to debug this script.
+my $verbose = "true";
+use File::Find;
+find (\&wanted, ("."));
+sub wanted
+{
+    if (/(.*\.(?:html|css|js)$)/) {
+        my $gz = "$_.gz";
+        if (-f $gz) {
+            if (age ($_) <= age ($gz)) {
+                if ($verbose) {
+                    print "Don't need to compress $_\n";
+                }
+                return;
+            }
+        }
+        if ($verbose) {
+            print "Compressing $_\n";
+        }
+        # The following substitution is for the case that the file
+        # name contains double quotation marks ".
+ 	$_ =~ s/"/\\"/g;
+        # Now compress the file.
+        system ("gzip --keep --best --force \"$_\"");
+    }
+    else {
+        if ($verbose) {
+            print "Rejecting $_\n";
+        }
+    }
+}
+
+# This returns the age of the file.
+
+sub age
+{
+    my ($file) = @_;
+    my @stat = stat $file;
+    return $stat[9];
+}
